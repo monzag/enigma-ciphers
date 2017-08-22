476e08867c54a29e31ccf80a7a9d0d8643bf1aa9
monzag <zaganiacz.m@gmail.com>
2017-08-22 14:29:03 +0200

    Add listAll

--- src/enigma/ServiceRepository.java
+++ src/enigma/ServiceRepository.java
@@ -2,6 +2,7 @@ package enigma
 
 import registry.ServiceProvider;
 import registry.ServiceRegistry;
+import services.EnigmaService;
 
 public class ServiceRepository {
 
@@ -14,9 +15,12 @@ public class ServiceRepository {
     
     }
 
-
     public List<String> listAll() {
-
+        List<String> allServices = new List<String>();
+        for (EnigmaService service ; services) {
+            allServices.add(service.getName());
+        }
+        return allServices;
     }
 
     public EnigmaService getByName(String name) {
