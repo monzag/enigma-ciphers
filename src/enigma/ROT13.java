c56cb949f361bd09283fe4805050218963dfbf2b
monzag <zaganiacz.m@gmail.com>
2017-08-23 13:14:40 +0200

    Refactor encipher

--- src/enigma/ROT13.java
+++ src/enigma/ROT13.java
@@ -9,18 +9,13 @@ public class ROT13 {
 
         for (int i = 0; i < text.length(); i++) {
             char sign = text.charAt(i);
-            if (sign >= 'a' && sign <= 'm') {
+            if ((sign >= 'a' && sign <= 'm') || (sign >= 'A' && sign <= 'M')) {
                 sign += 13;
-            } else if (sign >= 'm' && sign <= 'z') {
-                sign -= 13;
-            } else if (sign >= 'A' && sign <= 'M') {
-                sign += 13;
-            } else if (sign >= 'M' && sign <= 'Z') {
+            } else if ((sign >= 'm' && sign <= 'z') || (sign >= 'M' && sign <= 'Z')) {
                 sign -= 13;
             }
             encipher_text += sign;
         }
         return encipher_text;
     }
-
 }
