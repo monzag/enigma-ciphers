0e5386e354a5f3adf8e1c921f72f8517520cb12f
Ernest Jaroszek <ernestjaroszek22@gmail.com>
2017-08-23 14:49:36 +0200

    Atbash early sort, TerminalTranslator setkey

---
+++ src/enigma/Atbash.java
@@ -0,0 +1,23 @@
+package enigma;
+
+import services.*;
+
+public class Atbash{
+
+    String key;
+
+    public static String decipher(String toCipher){
+        StringBuffer deciphered = new StringBuffer;
+        for(char c; toCipher.toCharArray()){
+            if (Character.isLetter(c)){
+                int newChar = ("Z" - c) + "A";
+            }
+        }
+    }
+
+    public static String encipher(String toCipher){
+        StringBuffer enciphered = new StringBuffer;
+
+    }
+
+}
--- src/enigma/TerminalTranslator.java
+++ src/enigma/TerminalTranslator.java
@@ -9,27 +9,29 @@ import services.*;
 
 public class TerminalTranslator{
     ServiceRepository repo = null;
-    EnigmaService serviceName = null;
+    EnigmaService service = null;
     String mode = null;
+    String key = null;
     
     public void initialize(ServiceRepository repo){
         this.repo = repo;
     }
-    public void setParameters(String serviceName, String mode){
-        this.serviceName = repo.getByName(serviceName);
+    public void setParameters(String serviceName, String mode, String key){
+        this.service = repo.getByName(serviceName);
         this.mode = mode;
+        this.key = key;
     }
     public String getName(){
         return "Terminal Translator";
     }
-
     public void start(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Type in text");
         String toCipher = scanner.nextLine();
         cipher(toCipher);
     }
-    public void cipher(String toCipher) {
+    public void cipher(String toCipher){
+        service.setKey(key);
         if (mode.equals("-e")){
             String encipheredString = serviceName.encipher(toCipher);
             System.out.println(encipheredString);
@@ -38,5 +40,4 @@ public class TerminalTranslator{
             System.out.println(decipheredString);
         }
     }
-
 }
