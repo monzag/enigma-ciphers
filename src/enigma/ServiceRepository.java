fca5fa67cc849e57921c55835c20002bf5f0858f
monzag <zaganiacz.m@gmail.com>
2017-08-23 10:31:13 +0200

    Add ServiceRepository

--- src/enigma/ServiceRepository.java
+++ src/enigma/ServiceRepository.java
@@ -1,35 +1,39 @@
-package enigma
+package enigma;
 
 import registry.ServiceProvider;
 import registry.ServiceRegistry;
 import services.EnigmaService;
+import java.util.ArrayList;
 
 public class ServiceRepository implements ServiceProvider, ServiceRegistry {
 
     // <EnigmaService>??
-    public List<EnigmaService> services;
+    public ArrayList<EnigmaService> services;
 
     public ServiceRepository() {
-        services = new List<EnigmaService>();
+        services = new ArrayList<EnigmaService>();
         this.services = services;
     
     }
 
-    public List<String> listAll() {
-        List<String> allServices = new List<String>();
-        for (EnigmaService service ; services) {
+    public ArrayList<String> listAll() {
+        ArrayList<String> allServices = new ArrayList<String>();
+        for (EnigmaService service : services) {
             allServices.add(service.getName());
         }
         return allServices;
     }
 
     public EnigmaService getByName(String name) {
-        for (EnigmaService service ; services) {
+        EnigmaService result = null;
+        for (EnigmaService service : services) {
             if (service.getName() == name) {
-                return service;
+                result = service;
             }
         }
-    }
+        return result;
+    } 
+
 
     public void register(EnigmaService service) {
         services.add(service);
