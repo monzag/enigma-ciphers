8069b1adaa65765e2d2cfbf6f3f5fd742f521c26
monzag <zaganiacz.m@gmail.com>
2017-08-23 11:32:57 +0200

    Add getAscii

--- src/enigma/ROT13.java
+++ src/enigma/ROT13.java
@@ -4,4 +4,15 @@ import services.EnigmaService;
 
 public class ROT13 implements EnigmaService {
 
+    public static int[] getAscii() {
+        int ascii_amount = 256;
+        int[] values = new int[ascii_amount];
+
+        for (int i = 0; i < ascii_amount; i++) {
+            char temp = i;
+            values[i] = temp;
+        
+        return values;
+        }
+    }
 }
