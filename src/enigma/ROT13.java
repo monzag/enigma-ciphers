c732e7f6a66a64ad12bcb49397af19642aa27af7
monzag <zaganiacz.m@gmail.com>
2017-08-23 12:55:45 +0200

    Add encipher

--- src/enigma/ROT13.java
+++ src/enigma/ROT13.java
@@ -2,14 +2,33 @@ package enigma;
 
 import services.EnigmaService;
 
-public class ROT13 implements EnigmaService {
+public class ROT13 {
+
+    public String encipher(String text) {
+        String encipher_text = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char sign = text.charAt(i);
+            if (sign >= 'a' && sign <= 'm') {
+                sign += 13;
+            } else if (sign >= 'm' && sign <= 'z') {
+                sign -= 13;
+            } else if (sign >= 'A' && sign <= 'M') {
+                sign += 13;
+            } else if (sign >= 'M' && sign <= 'Z') {
+                sign -= 13;
+            }
+            encipher_text += sign;
+        }
+        return encipher_text;
+    }
 
     public static int[] getAscii() {
         int ascii_amount = 256;
         int[] values = new int[ascii_amount];
 
         for (int i = 0; i < ascii_amount; i++) {
-            char temp = i;
+            char temp = j;
             values[i] = temp;
         
         return values;
