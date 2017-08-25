f5e9ad799263c25a65f8dca9c43e3d254becf878
Iwo <iwo.tyszkowski@gmail.com>
2017-08-25 03:10:07 +0200

    Add encipher method for Caesar cipher

--- src/enigma/Caesar.java
+++ src/enigma/Caesar.java
@@ -4,5 +4,23 @@ import services.EnigmaService;
 
 public class Caesar implements EnigmaService {
 
-    ;
+    public String key;
+
+    public Caesar() {
+        this.key = null;
     }
+
+    public String encipher(String text) {
+        String eciphered = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char sign = text.charAt(i);
+
+            sign += 1;
+
+            eciphered += sign;
+        }
+        return eciphered;
+    }
+
+}
