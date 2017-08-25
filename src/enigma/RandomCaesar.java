2dd51e5c21c6a3125d1b18110b894239b73298c5
Iwo <iwo.tyszkowski@gmail.com>
2017-08-25 04:53:58 +0200

    Add encipher method for RandomCaesar class

--- src/enigma/RandomCaesar.java
+++ src/enigma/RandomCaesar.java
@@ -11,4 +11,23 @@ public class RandomCaesar implements EnigmaService {
         this.key = key;
     }
 
+    public String encipher(String text) {
+        String enciphered = "";
+        int min = -20;
+        int max = 20;
+        int randomNum = ThreadLocalRandom.current().nextInt(min, max);
+
+        for (int i = 0; i < text.length(); i++) {
+            char sign = text.charAt(i);
+
+            sign += randomNum;
+
+            enciphered += sign;
+        }
+        String key_str = Integer.toString(randomNum);
+        enciphered += " ----> and your key for decipher is: " + key_str;
+
+        return enciphered;
+    }
+
 }
